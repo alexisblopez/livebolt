0f4e52990fa66b7cac51971904e23ea6d0bad412
alexisblopez <alxs.lpz@gmail.com>
2017-03-30 13:32:34 -0500

    Alex4

--- server.js
+++ server.js
@@ -1,24 +1,26 @@
 var express = require('express');
-var mongoose = require('mongoose');
-var path = require('path')
+//var mongoose = require('mongoose');
+//var path = require('path')
 var app = express();   //aplication server object assigned to var app
 //var https = require('https');   //https used for more secure transmission 
 var http = require('http');
 var piblaster = require('pi-blaster.js');    //include pi-blaster for PWM of raspi
 
-
-
-
-
-mongoose.connect("mongodb://localhost/senior_design")
-require('./server/config/routes.js')(app);
+//mongoose.connect("mongodb://localhost/senior_design")
+//require('./server/config/routes.js')(app);
 
 app.use(express.static('public'));  //static files to be served , living in public folder
-app.use(express.static(path.join(__dirname, './client')));
-app.use(express.static(path.join(__dirname, './public/bower_components')));
+//app.use(express.static(path.join(__dirname, './client')));
+//app.use(express.static(path.join(__dirname, './public/bower_components')));
+
+//try a simpler rest get call
+app.get('/hello', function(req, res) { 
+       console.log("hello");
+ });
 
 //lock rest get call
 app.get('/lock', function(req, res) { 
+      console.log("lock")
       piblaster.setPwm(22, 0.145);
       res.end('Box is locked');
  });
@@ -43,9 +45,15 @@ app.use(function (err, req, res, next) {
       }
 });
 
-
 app.listen(8000, function(){
     console.log("Listening on port 3000")
+});
+
+on clrl-c, put stuff here to execute before closing your server with ctrl-c
+process.on('SIGINT', function() {
+ var i;
+ console.log("\nGracefully shutting down from SIGINT (Ctrl+C)");
+ process.exit();
 });
 
 //render template with the state of the lock
