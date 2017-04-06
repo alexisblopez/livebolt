2e1c3057d9095cd3ea06067c5f3d7a192b96dab8
Alexis <alxs.lpz@gmail.com>
2017-04-06 12:55:57 -0500

    YOLO

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 var express = require('express');
 var app = express();   //aplication server object assigned to var app
 var http = require('http');
-//var piblaster = require('pi-blaster.js');    //include pi-blaster for PWM of raspi
+var piblaster = require('pi-blaster.js');    //include pi-blaster for PWM of raspi
+var state = 0     // zero represents lock state, everytime server starts should be locked!
 
 app.use(express.static('public'));  //static files to be served , living in public folder
 
-app.get('/', function(req, res){
-  res.send('GET request to homepage')
+app.post('/lock', function(req, res){
+  console.log("LOCKED", req.body)
+  piblaster.setPwm(22, 0.145);    //setting pulse width modulation for locking
+  if (state == 0){
+    state = 1
+    piblaster.setPwm(22, 0.1); //setting the pulse width modulation for unlocking 
+  }
+  res.send('POST request to homepage')
 });
 
+//server needs to track state changes
+//connect server to database to check lock status
+
+//or make sure server knows it is in 1 specific state before issuing anything
+//make bool to keep trakc of state
 // Express route to handle errors
 app.use(function (err, req, res, next) {
       if (req.xhr) {
